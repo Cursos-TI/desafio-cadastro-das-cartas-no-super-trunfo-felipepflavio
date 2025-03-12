cf138e01826d775d6f9183fcf6c460a0ff08fddf
felipepflavio <felipepflavio@gmail.com>
2025-03-12 12:50:46 +0000

    Adicionando dados mais complexos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main(){
 
 char estado, codigoCarta [10], nomeCidade [30];
 int populacao, pontosTuristicos, c;
-float area, pib;
+float area, pib, densidadePopulacional, pibPerCapita;
 
 printf("JOGO SUPER TRUNFO. \n ");
 printf("————————————————————————————————————————————————————— \n");
@@ -19,27 +19,41 @@ for (c = 1; c < 3; c++)
     printf("CARTA %i", c,": \n");
     
     printf("Insira o estado, uma letra de: {A – H}: \n");
-    scanf("%c", &estado);
+    scanf("%c", &estado, "\n");
     
     printf("Insira o código da carta(o código da carta deve conter a letra escolhida na questão anterior seguida de um número, exe: A01): \n");
-    scanf("%s", &codigoCarta);
+    scanf("%s", &codigoCarta, "\n");
     
+    printf("\n");
+
     printf("Insira o nome da cidade(caso o nome da cidade seja de mais de uma palavra, essas palavras devem serem iseridas juntas! Exe: Rio de Janeiro – RioDeJaneiro.): \n");
-    scanf("%s", &nomeCidade);
+    scanf("%s", &nomeCidade, "\n");
+
+    printf("\n");
     
     printf("Insira o número da população habitante dessa cidade: \n");
-    scanf("%i", &populacao);
+    scanf("%i", &populacao, "\n");
+
+    printf("\n");
     
     printf("Insira a área da cidade em quilômetros quadrados: \n");
-    scanf("%f", &area);
+    scanf("%f", &area, "\n");
+
+    printf("\n");
     
     printf("Insira o PIB da cidade(Produto Interno Bruto): \n");
-    scanf("%f", pib);
+    scanf("%f", &pib, "\n");
+
+    printf("\n");
     
-    printf("Insira o número de pontos turísticos da cidade: \n");
-    scanf("%i", &pontosTuristicos);
+    printf("\n Insira o número de pontos turísticos da cidade: \n");
+    scanf("%i", &pontosTuristicos, "\n");
     
-    
+    // Realizando os cálculos da Densidade Populacional e do PIB per Capita;
+
+    densidadePopulacional = populacao / area;
+    pibPerCapita = pib / populacao;
+
     /*Imprimindo no terminal as cartas  com os dados inseridos pelo usuário:*/
     
     printf("————————————————————————————————————————————————————— \n");
@@ -49,12 +63,23 @@ for (c = 1; c < 3; c++)
     printf("\n");
 
     printf("ESTADO DA CARTA: %c", estado,"\n");
-    printf (" \n CÓDIGO DA CARTA: %s", codigoCarta,"\n");
-    printf(" \n NOME DA CIDADE: %s", nomeCidade,"\n");
-    printf(" \n NUMERO DA POPULAÇÃO DA CIDADE: %i", populacao,"\n");
-    printf(" \n ÁREA: %f", area,"\n");
-    printf("\n PIB: %f", pib,"\n");  
-    printf(" \n NUMEROS DE PONTOS TURISTICOS: %i", pontosTuristicos,"\n");
+    printf("\n");
+    printf ("CÓDIGO DA CARTA: %s", codigoCarta,"\n");
+    printf("\n");
+    printf("NOME DA CIDADE: %s", nomeCidade,"\n");
+    printf("\n");
+    printf("NUMERO DA POPULAÇÃO DA CIDADE: %i", populacao,"\n");
+    printf("\n");
+    printf("ÁREA: %f", area,"\n");
+    printf("\n");
+    printf("PIB: %f", pib,"\n");  
+    printf("\n");
+    printf("NUMEROS DE PONTOS TURISTICOS: %i", pontosTuristicos,"\n");
+    printf("\n");
+    printf("DENSIDADE POPULACIONAL: %f", densidadePopulacional, "\n");
+    printf("\n");
+    printf("PIB PER CAPITA: %f", pibPerCapita , "\n");
+    printf("\n");
 
     printf("————————————————————————————————————————————————————— \n");
 }
